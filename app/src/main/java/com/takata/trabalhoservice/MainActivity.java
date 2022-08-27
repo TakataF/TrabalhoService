a8cb7cfd5a89b5313fce36db254b5043b30c4577
VinciPy <viniciusantana323@gmail.com>
2022-08-26 22:28:12 -0300

    fix: main activity

--- app/src/main/java/com/takata/trabalhoservice/MainActivity.java
+++ app/src/main/java/com/takata/trabalhoservice/MainActivity.java
@@ -1,4 +1,14 @@
 package com.takata.trabalhoservice;
 
+import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
 
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
