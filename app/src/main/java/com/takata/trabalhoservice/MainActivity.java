652b0ccdaf15c79cea6818c7789323104c591ac2
VinciPy <viniciusantana323@gmail.com>
2022-08-26 23:27:52 -0300

    fix: sensor real

--- app/src/main/java/com/takata/trabalhoservice/MainActivity.java
+++ app/src/main/java/com/takata/trabalhoservice/MainActivity.java
@@ -2,20 +2,30 @@ package com.takata.trabalhoservice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     SeekBar seekBar;
     TextView TextViewLumLvl;
+    private SensorManager sensorManager;
+    private Sensor light;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        light = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
 
         seekBar = (SeekBar)findViewById(R.id.SeekBar);
         TextViewLumLvl = (TextView)findViewById(R.id.TextViewLumLvl);
@@ -36,6 +46,23 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        float value = sensorEvent.values[0];
+        Log.i("SENSOR", String.valueOf(value));
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, light, SensorManager.SENSOR_DELAY_NORMAL);
 
     }
 }
