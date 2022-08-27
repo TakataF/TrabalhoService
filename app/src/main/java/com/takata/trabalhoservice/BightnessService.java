45623881b5918e5ec8778cc821f448db986603f4
VinciPy <viniciusantana323@gmail.com>
2022-08-26 22:46:37 -0300

    feat: service empty

---
+++ app/src/main/java/com/takata/trabalhoservice/BightnessService.java
@@ -0,0 +1,16 @@
+package com.takata.trabalhoservice;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class BightnessService extends Service {
+    public BightnessService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
